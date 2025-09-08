ce0f5f88684cbb15e58c44d198aa2beca9372712
Roni Laukkarinen <roni@dude.fi>
2025-09-08 19:00:47 +0300

    Fix build

--- internal/config/config_test/config_test.go
+++ internal/config/config_test/config_test.go
@@ -56,6 +56,7 @@ func defaultConfig() *config.Config {
 			FlowBell:          true,
 			FlowBellSound:     "tibetan_bell",
 			FlowDefault:       true,
+			ConfettiEnabled:   false,
 		},
 		Notifications: config.NotificationConfig{
 			Enabled: true,
